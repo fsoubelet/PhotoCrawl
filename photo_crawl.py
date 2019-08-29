21f019f2229cfcd57d242dda916272f7ed5dfb23
Felix Soubelet <felix.soubelet@protonmail.com>
2019-08-29 11:45:48 +0100

    Slight formating. Better recursion.

--- photo_crawl.py
+++ photo_crawl.py
@@ -5,6 +5,7 @@ Created on 2019.08.15
 A simply script to run analysis and get insight on my use
 of equipment and settings in my practice of photography.
 """
+
 import pyexifinfo as pyexif
 import pandas as pd
 from glob import glob
@@ -12,47 +13,79 @@ from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
 from plotting_functions import plot_insight
 
+RAW_FORMATS: dict = {
+    "jpg": "The classic jpg file format",
+    "3FR": "Hasselblad 3F RAW Image",
+    "ARI": "ARRIRAW Image",
+    "ARW": "Sony Digital Camera Image",
+    "BAY": "Casio RAW Image",
+    "CR2": "Canon Raw Image File",
+    "CR3": "Canon Raw 3 Image File",
+    "CRW": "Canon Raw CIFF Image File",
+    "CS1": "CaptureShop 1-shot Raw Image",
+    "CXI": "FMAT RAW Image",
+    "DCR": "Kodak RAW Image File",
+    "DNG": "Digital Negative Image File",
+    "EIP": "Enhanced Image Package File",
+    "ERF": "Epson RAW File",
+    "FFF": "Hasselblad RAW Image",
+    "IIQ": "Phase One RAW Image",
+    "J6I": "Ricoh Camera Image File",
+    "K25": "Kodak K25 Image",
+    "KDC": "Kodak Photo-Enhancer File",
+    "MEF": "Mamiya RAW Image",
+    "MFW": "Mamiya Camera Raw File",
+    "MOS": "Leaf Camera RAW File",
+    "MRW": "Minolta Raw Image File",
+    "NEF": "Nikon Electronic Format RAW Image",
+    "NRW": "Nikon Raw Image File",
+    "ORF": "Olympus RAW File",
+    "PEF": "Pentax Electronic File",
+    "RAF": "Fuji RAW Image File",
+    "RAW": "Raw Image Data File",
+    "RW2": "Panasonic RAW Image",
+    "RWL": "Leica RAW Image",
+    "RWZ": "Rawzor Compressed Image",
+    "SR2": "Sony RAW Image",
+    "SRF": "Sony RAW Image",
+    "SRW": "Samsung RAW Image",
+    "X3F": "SIGMA X3F Camera RAW File",
+}
 
-INTERESTING = ['DateTimeOriginal',
- 'ExposureCompensation',
- 'ExposureProgram',
- 'FNumber',
- 'Flash',
- 'FocalLengthIn35mmFormat',
- 'ISO',
- 'LensMake',
- 'LensModel',
- 'Make',
- 'MeteringMode',
- 'Model',
- 'ShutterSpeedValue',
- 'WhiteBalance',
+INTERESTING_FEATURES: list = [
+    "DateTimeOriginal",
+    "ExposureCompensation",
+    "ExposureProgram",
+    "FNumber",
+    "Flash",
+    "FocalLengthIn35mmFormat",
+    "ISO",
+    "LensMake",
+    "LensModel",
+    "Make",
+    "MeteringMode",
+    "Model",
+    "ShutterSpeedValue",
+    "WhiteBalance",
 ]
 
 
 def get_exif(file_path: str) -> dict:
     """Returns a dict with interesting features from image EXIF."""
-    return {key[5:]: value for key, value in pyexif.get_json(file_path)[0].items() if key[5:] in INTERESTING}
+    return {key[5:]: value for key, value in pyexif.get_json(file_path)[0].items() if key[5:] in INTERESTING_FEATURES}
 
 
 def process_files() -> pd.DataFrame:
     rootpath = input("Absolute UNIX path to files location: ")
     images = (
-        glob(rootpath + "/*.jpg")
-        + glob(rootpath + "/**/*.jpg")
-        + glob(rootpath + "/**/*.JPG")
+        glob(rootpath + f"/*.jpg", recursive=True)
+        + glob(rootpath + "/**/*.jpg", recursive=True)
+        + glob(rootpath + "/**/*.JPG", recursive=True)
         + glob(rootpath + "/**/*.jpeg", recursive=True)
     )
     with Pool(cpu_count()) as pool:
         metadata = pd.DataFrame(
-            list(
-                tqdm(
-                    pool.imap_unordered(get_exif, images),
-                    desc="Scraping Images",
-                    total=len(images),
-                    unit=" Files",
-                )
-            )
+            list(tqdm(pool.imap_unordered(get_exif, images), desc="Scraping Images", total=len(images), unit=" Files"))
         )
         return metadata
 
@@ -105,35 +138,50 @@ def rework_data(exif_dataframe: pd.DataFrame) -> pd.DataFrame:
     temp_df["Year"] = temp_df.DateTimeOriginal.str[:4]
     temp_df["Month"] = temp_df.DateTimeOriginal.str[5:7]
     temp_df["Day"] = temp_df.DateTimeOriginal.str[8:10]
-    
+
     # Determine focal length ranges
     temp_df.Focal_Length = temp_df.Focal_Length.apply(lambda x: int(str(x[:3])))
     temp_df["Focal_Range"] = temp_df.apply(lambda row: figure_focal_range(row), axis=1)
-    
+
     # Make categorical
-    for col in ["Exposure_Compensation", "Exposure_Program", "Flash", "Lens_Brand", "Lens", "Brand", "Metering_Mode", "Camera", "Shutter_Speed", "White_Balance", "Focal_Range"]:
+    for col in [
+        "Exposure_Compensation",
+        "Exposure_Program",
+        "Flash",
+        "Lens_Brand",
+        "Lens",
+        "Brand",
+        "Metering_Mode",
+        "Camera",
+        "Shutter_Speed",
+        "White_Balance",
+        "Focal_Range",
+    ]:
         temp_df.loc[:, col] = temp_df.loc[:, col].astype("category")
-    
+
     # Clarify some labels. Shouldn't stay here too long as I can't cover all lenses anyway.
     temp_df.Lens = temp_df.Lens.cat.remove_unused_categories()
-    temp_df["Metering_Mode"] = temp_df["Metering_Mode"].map(
-        {
-            "Center-weighted average": "Center Weighted",
-            "Multi-segment": "Multi Segment",
-        }
-    ).fillna(temp_df["Metering_Mode"])
-    temp_df["Lens"] = temp_df["Lens"].map(
-        {
-            "XF10-24mmF4 R OIS": "XF 10-24mm f/4 R",
-            "XF18-135mmF3.5-5.6R LM IOS WR": "XF 18-135mm f/3.5-5.6 R LM OIS WR",
-            "XF18-55mmF2.8-4 R LM OIS": "XF 18-55mm f/2.8-4 R LM OIS",
-            "XF23mmF1.4 R": "XF 23mm f/1.4 R",
-            "XF50-140mmF2.8 R LM OIS WR": "XF 50-140mm f/2.8 R LM OIS WR",
-            "XF50-140mmF2.8 R LM OIS WR + 1.4x": "XF 50-140mm f/2.8 R LM OIS WR + 1.4x",
-            "XF55-200mmF3.5-4.8 R LM OIS": "XF 55-200mm f/3.5-4.8 R LM OIS",
-            "XF56mmF1.2 R APD": "XF 56mm f/1.2 R APD",
-        }
-    ).fillna(temp_df["Lens"])
+    temp_df["Metering_Mode"] = (
+        temp_df["Metering_Mode"]
+        .map({"Center-weighted average": "Center Weighted", "Multi-segment": "Multi Segment"})
+        .fillna(temp_df["Metering_Mode"])
+    )
+    temp_df["Lens"] = (
+        temp_df["Lens"]
+        .map(
+            {
+                "XF10-24mmF4 R OIS": "XF 10-24mm f/4 R",
+                "XF18-135mmF3.5-5.6R LM IOS WR": "XF 18-135mm f/3.5-5.6 R LM OIS WR",
+                "XF18-55mmF2.8-4 R LM OIS": "XF 18-55mm f/2.8-4 R LM OIS",
+                "XF23mmF1.4 R": "XF 23mm f/1.4 R",
+                "XF50-140mmF2.8 R LM OIS WR": "XF 50-140mm f/2.8 R LM OIS WR",
+                "XF50-140mmF2.8 R LM OIS WR + 1.4x": "XF 50-140mm f/2.8 R LM OIS WR + 1.4x",
+                "XF55-200mmF3.5-4.8 R LM OIS": "XF 55-200mm f/3.5-4.8 R LM OIS",
+                "XF56mmF1.2 R APD": "XF 56mm f/1.2 R APD",
+            }
+        )
+        .fillna(temp_df["Lens"])
+    )
 
     temp_df.dropna(inplace=True)
     return temp_df
